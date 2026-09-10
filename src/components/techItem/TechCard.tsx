75cbf93ef51a0598306bdd285f0bb1371f65402b
Nur Alam <nuralam1122345@gmail.com>
2026-09-11 00:18:25 +0600

    diploy done

--- src/components/techItem/TechCard.tsx
+++ src/components/techItem/TechCard.tsx
@@ -1,14 +1,22 @@
-import type { Technology } from "../../types";
+import type { Technology } from '../../types';
 
 interface TechCardProps {
   tech: Technology;
   isSelected: boolean;
   onAddToStack: (tech: Technology) => void;
 }
+
 const TechCard = ({ tech, isSelected, onAddToStack }: TechCardProps) => {
   return (
-    <div className="bg-white rounded-2xl p-5 border border-gray-100 shadow-sm flex flex-col justify-between hover:shadow-md transition">
+    <div
+      className={`bg-white rounded-2xl p-5 border transition flex flex-col justify-between hover:shadow-md ${
+        isSelected
+          ? 'border-purple-300 ring-1 ring-purple-300 shadow-sm'
+          : 'border-gray-100 shadow-sm'
+      }`}
+    >
       <div>
+        {/* Header */}
         <div className="flex justify-between items-center mb-4">
           <img
             src={tech.image}
@@ -19,11 +27,15 @@ const TechCard = ({ tech, isSelected, onAddToStack }: TechCardProps) => {
             {tech.badge}
           </span>
         </div>
+
+        {/* Info */}
         <h3 className="text-lg font-bold text-gray-900 mb-1">{tech.name}</h3>
         <p className="text-xs text-gray-500 line-clamp-3 leading-relaxed mb-6">
           {tech.description}
         </p>
       </div>
+
+      {/* Footer Info & Action */}
       <div>
         <div className="flex items-center justify-between text-xs text-gray-500 mb-4 pt-2 border-t border-gray-50">
           <span className="bg-gray-100 px-2 py-0.5 rounded text-gray-600 font-medium">
@@ -34,13 +46,14 @@ const TechCard = ({ tech, isSelected, onAddToStack }: TechCardProps) => {
             <span className="text-amber-400 mr-1">★</span> {tech.rating}
           </span>
         </div>
+
         <button
           onClick={() => onAddToStack(tech)}
           disabled={isSelected}
           className={`w-full py-2.5 rounded-xl font-medium text-xs transition flex items-center justify-center gap-1 ${
             isSelected
-              ? "bg-purple-50 text-purple-600 border border-purple-100 cursor-not-allowed"
-              : "bg-slate-900 hover:bg-black text-white"
+              ? 'bg-purple-50 text-purple-600 border border-purple-100 cursor-not-allowed'
+              : 'bg-slate-900 hover:bg-black text-white'
           }`}
         >
           {isSelected ? (
